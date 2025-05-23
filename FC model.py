4f318cc68b023d5297b0e5e208d5a179d43833bc
talkahal <84787446+talkahal@users.noreply.github.com>
2025-05-23 23:38:46 +0300

    modified:   FC model.py 	new file:   for_DPD.mat

--- FC model.py
+++ FC model.py
@@ -1,42 +1,60 @@
 
 import torch
-import torchvision
+import torch.nn as nn
+import torch.optim as optim 
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import math
-import scipy.io # Library to load the mat file
+from scipy.io import loadmat # for mat files
 
 # Fully connected model
+#First FC is the linear to 40 neurons 
+#Second FC is the 
 
 class FC_Model(nn.Module):
     
     def __init__(self):
         super().__init__()
-        # Hidden layer, 6->40. input of 3 timesteps, real and imagenary
-        self.hidden = nn.Linear(6, 40)
-        ## Output of one timestep, real and imagenary
-        self.output = nn.Linear(40, 2)
-        self.relu = nn.ReLU()             
+        self.fc1 = nn.Linear(6, 40) #input layer : 6 inputs -> 40 neurons
+        self.relu = nn.ReLU() #ReLU activation  
+        self.fc2 = nn.Linear(40, 2)  #Hidden layer 40 -> 2 outputs           
     def forward(self, x):
-        x = self.relu(self.hidden(x)) #Activate function just on the hidden layer    
-        x = self.output(x)                
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)                
         return x
 
 
 class Signal_Dataset(Dataset):
 
-    def __init__(self):
+    def __init__(self,M=3):
         # Load and initialize the data
-        file_path = r'C:\Users\User\Desktop\for_DPD.mat' # Change it to your path
-        mat = scipy.io.loadmat(file_path)
-        TX1_BB = mat['TX1_BB'] # I left names from the file because i dont know what is the original signal
-        TX1_SISO = mat['TX1_SISO']
-        # Decomposition into real and imaginary parts
-        self.TX1_BB_real = torch.from_numpy(TX1_BB.real)
-        self.TX1_SISO_real = torch.from_numpy(TX1_SISO.real)
-        self.TX1_BB_imag = torch.from_numpy(TX1_BB.imag)
-        self.TX1_SISO_imag = torch.from_numpy(TX1_SISO.imag)
-        self.n_samples = TX1_BB.shape[0]
+        mat = loadmat('for_DPD.mat')
+        #X_n is the baseband signal TX1_BB
+        # Y_n is the recorded distorted signal TX1_SISO 
+        X = mat['TX1_BB'].squeeze()
+        Y = mat['TX1_SISO'].squueze()
+        self.M= M
+
+        inputs=[]
+        targets=[]
+
+        #prepare the data to the FC_Model
+        for n in range(M-1,len(X)):
+            x_taps = X[n-M:-1]
+            if len(x_taps)<M:
+                continue
+            input_vec=np.empty(2*M,dtype=np.float32)
+            input_vec[0::2]=np.real(x_taps)
+            input_vec[1::2]=np.imag(x_taps)
+            inputs.append(input_vec)
+            targets.append([np.real(Y[n],np.imag(Y[n]))])
+
+
+        # Decomposition in tensors
+        self.x_data = torch.tensor(np.stack(inputs), dtype=torch.float32)
+        self.y_data = torch.tensor(np.stack(targets), dtype=torch.float32)
+        self.n_samples = self.x_data.shape[0]
 
     #  From here it is just copy paste from the gitHub of the course
     # support indexing such that dataset[i] can be used to get i-th sample
@@ -49,5 +67,25 @@ class Signal_Dataset(Dataset):
 
 
 # create dataset and model
-dataset = Signal_Dataset()
+dataset = Signal_Dataset(M=3)
+loader= DataLoader(dataset,batch_size=64,shuffle=True)
 model = FC_Model()
+
+#set up training tools / ADAM = adaptive Moment estimation
+loss_function=nn.MSELoss()
+optimizer=optim.Adam(model.parameters(), lr=1e-3)
+
+#training loop template 
+n_epochs=20
+
+for epoch in range(n_epochs):
+    #Forward pass
+    for batch_in, batch_tar in loader:
+        outputs=model(batch_in)
+        loss=loss_function(outputs,batch_tar)
+    #Backward pass
+    optimizer.zero_grad() #important!
+    loss.backward()
+    optimizer.step()
+
+print(f"Epoch {epoch+1},Loss: {loss.item():.6f}")
